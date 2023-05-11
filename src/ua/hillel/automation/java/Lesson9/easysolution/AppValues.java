bf2789bbaa3245cf36d013f4238ecebc5e07c6fb
Анжела <anzhela.chala@nixs.com>
2023-05-11 19:43:15 +0300

    Lesson 9 material

---
+++ src/ua/hillel/automation/java/Lesson9/easysolution/AppValues.java
@@ -0,0 +1,9 @@
+package ua.hillel.automation.java.Lesson9.easysolution;
+//контейнер, який може включати багато сталих значень (декілька enum)
+public class AppValues {
+    public enum ProductStatus {
+        NEW,
+        OUT_OF_SALE,
+        WAITING;
+    }
+}
---
+++ src/ua/hillel/automation/java/Lesson9/easysolution/NextedClassEx.java
@@ -0,0 +1,14 @@
+package ua.hillel.automation.java.Lesson9.easysolution;
+
+public class NextedClassEx {
+    public static void main(String[] args) {
+        //щоб звернутись до внутрішноього класу зазначаємо ім'я класу контейнера
+        ProductTest.Product product = new ProductTest.Product();
+        product.getName();
+
+        //доступ до enum всередині класу
+
+        System.out.println(AppValues.ProductStatus.WAITING);
+
+    }
+}

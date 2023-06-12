a3925b038045e888c61999ec8e2f5621ccd482be
Анжела <anzhela.chala@nixs.com>
2023-06-12 16:58:47 +0300

    lesson 15 testNG

---
+++ src/test/java/ua/hillel/tests/beforeAfterTests/Test1.java
@@ -0,0 +1,32 @@
+package ua.hillel.tests.beforeAfterTests;
+
+import org.testng.annotations.*;
+
+public class Test1 {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("==== Before Class test1 ====");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("==== After Class test1 ====");
+
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("==== Before Method ====");
+
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("==== After Method ====");
+
+    }
+    @Test
+    public void test1() {
+        System.out.println("==== Test 1 =====");
+    }
+    public void test2() {
+        System.out.println("==== Test 2 =====");
+    }
+}
---
+++ src/test/java/ua/hillel/tests/data/DataProviderTest.java
@@ -0,0 +1,47 @@
+package ua.hillel.tests.data;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class DataProviderTest {
+    @Test(dataProvider = "userProvider")
+    public void userRoleTest(String userName,String password, String role){
+        System.out.printf("name: %s - password: %s - role: %s%n", userName,password,role);
+        System.out.println("Starting test for " + role);
+        if(role.equals("admin")) {
+            throw new RuntimeException("Test failed"); //імітація помилки
+        }
+        System.out.println("Test for " + role + " passed");
+
+        //Assert
+        //тут можуть бути різні типи
+        Assert.assertEquals("password1234","1234", "Assertion message");
+        Assert.assertFalse(true,"Message");
+
+        //SoftAssert
+        SoftAssert softUserAssert = new SoftAssert();
+        softUserAssert.assertEquals(userName,"Jon");
+        softUserAssert.assertTrue(role.isBlank());
+        //запуск наших SoftAssert
+
+        System.out.println("Test goes on 1");
+        softUserAssert.assertAll();
+
+
+        System.out.println("Test goes on 2");
+
+    }
+    //метод повертає набір тестових данних, тому повертаємий тип - масив масивів об'єктів
+    //зовнішній масив(перший) - тестові данні, і в середині - різні об'єкти
+    @DataProvider(name = "userProvider")
+    public Object[][] userProvider(){
+        String[][] users = {
+                {"userName", "userPass", "user"},
+                {"adminName", "adminPass", "admin"},
+                {"guestName", "guestPass", "guest"},
+        };
+        return users;
+    }
+}
